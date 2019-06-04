ea85d0549956e1495173f685a37f7f3462aeb7fd
zazapeta <sami.ghazouane@gmail.com>
2019-06-04 18:02:14 +0200

    Add GET /user/2

--- resources/users/user.routes.js
+++ resources/users/user.routes.js
@@ -1,9 +1,23 @@
 const User = require("./user.model");
 
-module.exports = {
-  method: "GET",
-  path: "/users",
-  handler: function() {
-    return User.findAll();
+module.exports = [
+  {
+    method: "GET",
+    path: "/users",
+    handler: function() {
+      return User.findAll();
+    }
+  },
+  {
+    method: "GET",
+    path: "/users/{id}",
+    handler: function(request) {
+      const userId = request.params.id;
+      return User.findOne({
+        where: {
+          id: userId
+        }
+      });
+    }
   }
-};
+];
--- test/users.js
+++ test/users.js
@@ -12,42 +12,62 @@ describe("# USERS", () => {
   afterEach(async () => {
     await server.stop();
   });
-
-  it("responds with 200", async () => {
-    const res = await server.inject({
-      method: "GET",
-      url: "/users"
+  describe("## GET /users/{id}", () => {
+    it("responds with the user given id", async () => {
+      const user1 = {
+        email: "testuserone@toto.com",
+        password: "abc",
+        name: "userone",
+        id: 1
+      };
+      await User.create(user1);
+      const res = await server.inject({
+        method: "GET",
+        url: "/users/1"
+      });
+      should(res.statusCode).equal(200);
+      const payload = JSON.parse(res.payload);
+      should(payload).match(user1);
     });
-    should(res.statusCode).equal(200);
   });
 
-  it("responds an empty user list", async () => {
-    const res = await server.inject({
-      method: "GET",
-      url: "/users"
+  describe("## GET /users", () => {
+    it("responds with 200", async () => {
+      const res = await server.inject({
+        method: "GET",
+        url: "/users"
+      });
+      should(res.statusCode).equal(200);
     });
-    const payload = JSON.parse(res.payload);
-    should(payload).deepEqual([]);
-  });
 
-  it("responds a user list with two users", async () => {
-    const user1 = {
-      email: "testuserone@toto.com",
-      password: "abc",
-      name: "userone"
-    };
-    const user2 = {
-      email: "testusertwo@toto.com",
-      password: "def",
-      name: "usertwo"
-    };
-    await User.create(user1);
-    await User.create(user2);
-    const res = await server.inject({
-      method: "GET",
-      url: "/users"
+    it("responds an empty user list", async () => {
+      const res = await server.inject({
+        method: "GET",
+        url: "/users"
+      });
+      const payload = JSON.parse(res.payload);
+      should(payload).deepEqual([]);
     });
-    const payload = JSON.parse(res.payload);
-    should(payload).match([user1, user2]);
+
+    it("responds a user list with two users", async () => {
+      const user1 = {
+        email: "testuserone@toto.com",
+        password: "abc",
+        name: "userone"
+      };
+      const user2 = {
+        email: "testusertwo@toto.com",
+        password: "def",
+        name: "usertwo"
+      };
+      await User.create(user1);
+      await User.create(user2);
+      const res = await server.inject({
+        method: "GET",
+        url: "/users"
+      });
+      const payload = JSON.parse(res.payload);
+      should(payload).match([user1, user2]);
+    });
   });
 });
